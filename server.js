2646c2969d65dc3a3faa438a2913e130317d6e9d
gsgs0404900-star <gsgs0404900@gmail.com>
2026-09-01 16:44:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -458,35 +458,6 @@ app.post(
 );
 
 /* =========================================================
-   MINEGRAM - EMAIL OTP
-   POST /api/send-email-otp
-========================================================= */
-
-const otpStore = new Map();
-
-const mailTransporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST || "smtp.gmail.com",
-
-    port: Number(
-        process.env.SMTP_PORT || 465
-    ),
-
-    secure:
-        String(
-            process.env.SMTP_SECURE || "true"
-        ).toLowerCase() === "true",
-
-    auth: {
-        user:
-            process.env.SMTP_USER,
-
-        pass:
-            process.env.SMTP_PASS
-    }
-});
-
-
-/* =========================================================
    SMTP TEST
 ========================================================= */
 
